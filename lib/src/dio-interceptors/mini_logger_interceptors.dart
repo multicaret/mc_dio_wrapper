54dbd5fa13943af49c2030468c1d4c9d71eff7ce
suhailkawsara <msk286@gmail.com>
2024-11-27 01:57:48 +0300

    Refactor logging and add response interceptor
    Refactor request logging to use private `_logger` method. Add response interceptor to log status code and response details.
    
    Signed-off-by: suhailkawsara <msk286@gmail.com>

--- lib/src/dio-interceptors/mini_logger_interceptors.dart
+++ lib/src/dio-interceptors/mini_logger_interceptors.dart
@@ -1,4 +1,5 @@
-import 'dart:developer';
+import 'dart:async';
+import 'dart:developer' as main_logger show log;
 
 import 'package:dio/dio.dart';
 import 'package:mc_dio_wrapper/src/helpers/package_keys.dart';
@@ -8,11 +9,38 @@ class MiniLoggerInterceptor extends QueuedInterceptor {
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    log('➡️ ➡️ ➡️ Request', name: PackageKeys.name);
-    log('${options.method.toUpperCase()}:${options.uri.toString()}', name: PackageKeys.name);
+    _logger('➡️ ➡️ ➡️ Request', name: PackageKeys.name);
+    _logger('${options.method.toUpperCase()}:${options.uri.toString()}', name: PackageKeys.name);
     options.headers.forEach((String key, dynamic value) {
-      log('𝍌 Headers: $key->${value.toString()}', name: PackageKeys.name);
+      _logger('𝍌 Headers: $key->${value.toString()}', name: PackageKeys.name);
     });
     return super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    _logger('⬅️ ⬅️ ⬅️ Response', name: PackageKeys.name);
+    final String statusCode = response.statusCode != 200 ? '❌ ${response.statusCode} ❌' : '✅ 200 ✅';
+    _logger('$statusCode ${response.requestOptions.uri.toString()}', name: PackageKeys.name);
+    _logger('-------------------------', name: PackageKeys.name);
+    return handler.next(response);
+  }
+
+  void _logger(String message,
+      {Object? error,
+      int level = 0,
+      String name = '',
+      int? sequenceNumber,
+      StackTrace? stackTrace,
+      DateTime? time,
+      Zone? zone}) {
+    main_logger.log(message,
+        name: name,
+        error: error,
+        level: level,
+        sequenceNumber: sequenceNumber,
+        stackTrace: stackTrace,
+        time: time,
+        zone: zone);
   }
 }

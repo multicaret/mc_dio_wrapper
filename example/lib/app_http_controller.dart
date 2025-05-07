faf75766cd3e820424e09649d304d230d9dc9020
suhailkawsara <msk286@gmail.com>
2025-05-07 14:14:38 +0300

    refactor app_http_controller to update http service init
    Replaced the initialization of `McHttpWrapper` to include a default locale ('ar'). Also updated the query parameters class from `HttpRequestPayload` to `RequestQueryParams` for better alignment with naming conventions.
    
    Signed-off-by: suhailkawsara <msk286@gmail.com>

--- example/lib/app_http_controller.dart
+++ example/lib/app_http_controller.dart
@@ -4,14 +4,14 @@ import 'package:mc_dio_wrapper/mc_dio_wrapper.dart';
 import 'api_endpoints.dart';
 
 class AppHttpController {
-  final McHttpWrapper _httpService = McHttpWrapper();
+  final McHttpWrapper _httpService = McHttpWrapper('ar');
 
   Future<List<User>> fetchUsers() async {
     try {
       McResponse<UserIndexResponse> response = await _httpService.get<UserIndexResponse>(
         AppApiEndpoints.userIndex.path,
         converter: UserIndexResponse.fromJson,
-        queryParameters: HttpRequestPayload.fromMap({'limit': 2}),
+        queryParameters: RequestQueryParams.fromMap({'limit': 2}),
       );
 
       return response.data!.data.users;
